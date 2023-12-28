34c28dae8821b3fac9b565351ad2a935ad05aa62
rootware <Shah.Saad.Alam@rice.edu>
2023-12-28 14:27:06 -0700

    Run Option1 with restricted params

--- src/Generate.cpp
+++ src/Generate.cpp
@@ -13,17 +13,19 @@ const std::vector<double> shakingfunctions {3.92699082, 3.92699082, 3.40339204, 
 const std::vector<double> shakingfunctions2p2e {3.40339204, 3.92699082, 1.83259571, 1.57079633, 2.0943951, 1.57079633, 2.61799388, 0., 3.92699082, 3.92699082,
 2.0943951, 2.35619449, 2.35619449, 2.35619449, 2.35619449, 3.92699082, 3.92699082, 3.92699082, 2.61799388,
 3.40339204, 3.92699082, 3.66519143, 3.66519143, 3.66519143, 3.66519143, 3.66519143, 3.66519143, 0.78539816,
-0.78539816, 0.78539816, 0.78539816, 0.78539816}; // acceleration as 2 parameter
+0.78539816, 0.78539816, 0.78539816, 0.78539816}; // acceleration as 2 parameter, Option 1
 
 
 const std::vector<double> shakingfunctions2p2e_new {1.83259571, 0., 1.83259571, 2.87979327, 1.83259571, 1.83259571, 1.83259571, 3.40339204, 3.66519143,
 3.40339204, 3.40339204, 3.14159265, 3.92699082, 3.92699082, 2.35619449, 2.35619449, 3.92699082, 3.92699082,
 3.92699082, 3.66519143, 3.66519143, 3.66519143, 2.61799388, 3.66519143, 1.57079633, 1.57079633, 1.57079633,
 1.04719755, 1.04719755, 1.04719755, 1.04719755, 1.57079633
-};
+}; // Option2 from paper
 
 
-const std::vector<double> shakingfunction2platt {1.83259571, 1.83259571, 1.83259571, 1.83259571, 1.83259571, 1.83259571, 1.04719755, 1.04719755, 1.04719755,0., 0., 0.52359878, 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.26179939, 0., 0., 0., 0., 0., 0., 0., 0};
+const std::vector<double> shakingfunction2platt {1.83259571, 1.83259571, 1.83259571, 1.83259571, 1.83259571,
+ 1.83259571, 1.04719755, 1.04719755, 1.04719755,0., 0., 0.52359878, 
+ 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.26179939, 0., 0., 0., 0., 0., 0., 0., 0};// Lattice sensitive interferometer sequence
 
 const int maxSteps = shakingfunctions.size();
 
@@ -35,10 +37,11 @@ constexpr double acclim = 0.1 * ACC_NORM;  // dacc is the epsilon in acceleratio
 
 int main()
 { 
-    Eigen::VectorXd a_list = Eigen::VectorXd::LinSpaced(101,-0.0225,0.0225);
-    Eigen::VectorXd V_list = Eigen::VectorXd::LinSpaced(51, 9.0, 11.0);
+    // Eigen::VectorXd a_list = Eigen::VectorXd::LinSpaced(101,-0.0225,0.0225);
+    Eigen::VectorXd a_list = Eigen::VectorXd::LinSpaced(101,-0.00446,0.00446);
+    Eigen::VectorXd V_list = Eigen::VectorXd::LinSpaced(51, 9.5, 10.5);
 
-    std::vector<double> currentshaking = shakingfunctions2p2e_new;
+    std::vector<double> currentshaking = shakingfunctions2p2e; // Option 1
 
   //  Eigen::VectorXd V_list(1) ;
   //  V_list <<10.0;
@@ -57,8 +60,8 @@ int main()
         for (int Vindex =0; Vindex < V_list.size() ; Vindex++){
             Wavepacket wp (a_list[aindex], V_list[Vindex]);
             int sign = 1;
-            for (int i=0; i< int(shakingfunctions2p2e.size()); i++){
-                wp.step(sign *shakingfunctions2p2e[i], omega);
+            for (int i=0; i< int(currentshaking.size()); i++){
+                wp.step(sign *currentshaking[i], omega);
                 sign = -1*sign;
             };
 
